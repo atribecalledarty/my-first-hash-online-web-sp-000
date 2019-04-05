a8aa056e9ea1d26fa2243dd04203690111811c06
Arthur Song <arth3rs0ng@gmail.com>
2019-04-05 05:07:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = {
+    "name" => "Arthur",
+    "age" => 23
+  }
   
 end
 
